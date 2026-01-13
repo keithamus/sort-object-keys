110409f29e09f4959d0ffd8555b5b2385b5cf0ef
Hyper-Z11 <114817308+hyperz111@users.noreply.github.com>
2026-01-14 01:22:30 +0700

    test: use Object.keys + assert.deepEqual (#21)

--- index.test.js
+++ index.test.js
@@ -1,40 +1,46 @@
 import { test } from 'node:test'
 import sortObject from './index.js'
 
+const { keys } = Object
+
 test('sort keys by default', ({ assert }) => {
   assert.deepEqual(
-    sortObject({
-      c: 1,
-      b: 1,
-      d: 1,
+    keys(
+      sortObject({
+        c: 1,
+        b: 1,
+        d: 1,
+        a: 1,
+      }),
+    ),
+    keys({
       a: 1,
+      b: 1,
+      c: 1,
+      d: 1,
     }),
-    {
-      a: 1,
-      b: 1,
-      c: 1,
-      d: 1,
-    },
   )
 })
 
 test('using an array of ordered keys', ({ assert }) => {
   assert.deepEqual(
-    sortObject(
-      {
-        c: 1,
-        b: 1,
-        d: 1,
-        a: 1,
-      },
-      ['b', 'a', 'd', 'c'],
+    keys(
+      sortObject(
+        {
+          c: 1,
+          b: 1,
+          d: 1,
+          a: 1,
+        },
+        ['b', 'a', 'd', 'c'],
+      ),
     ),
-    {
+    keys({
       b: 1,
       a: 1,
       d: 1,
       c: 1,
-    },
+    }),
   )
 })
 
@@ -46,28 +52,29 @@ test('using comparator function', ({ assert }) => {
   }
 
   assert.deepEqual(
-    sortObject(
-      {
-        'key-1': 1,
-        'key-3': 1,
-        'key-10': 1,
-        'key-2': 1,
-      },
-      comparator,
+    keys(
+      sortObject(
+        {
+          'key-1': 1,
+          'key-3': 1,
+          'key-10': 1,
+          'key-2': 1,
+        },
+        comparator,
+      ),
     ),
-
-    {
+    keys({
       'key-1': 1,
       'key-2': 1,
       'key-3': 1,
       'key-10': 1,
-    },
+    }),
   )
 })
 
 test('using array with some not-contained keys', ({ assert }) => {
   assert.deepEqual(
-    Object.keys(
+    keys(
       sortObject(
         {
           b: 1,
